eddd3b62fc0a33c284aaec61216209c4b990c036
Gabriel <joaogabrielonofre@gmail.com>
2020-09-11 18:34:16 -0300

    Add XP Table from DnD 5e and create command to show the table

---
+++ commands/xpAll.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+module.exports = {
+  name: 'xpall',
+  description: 'Show XP in all the levels',
+  aliases: ['xptodos'],
+  usage: `[command]`,
+  cooldown: 10,
+  execute(message) {
+    //getting the data from the json file
+    let data = fs.readFileSync('./util/xp-5e.json');
+    let xp = JSON.parse(data);
+    let displayXP = '';
+
+    //mapping through the data and creating a formatted string with its content
+    xp.map((item) => {
+      displayXP += `Level: ${item.level} - XP: ${item.xp}\n`;
+    });
+
+    message.channel.send(displayXP);
+  },
+};
